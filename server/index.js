35609119886c55fdd9d56df310731d7160c92511
Rocky4869 <rockytam4869@gmail.com>
2024-12-07 22:39:11 +0800

    server setup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://csci2720:csci2720@cluster0.fbcue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+(async () => {
+  await connectDB();
+})();
