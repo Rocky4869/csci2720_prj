e9f4edd6445f201d8509287b2b29906e204ca779
Rocky4869 <rockytam4869@gmail.com>
2024-12-16 01:29:57 +0800

    google map fixed zoom it out

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  InfoWindow,
-} from "@vis.gl/react-google-maps";
-
-const NEXT_PUBLIC_GOOGLE_MAPS_API_KEY =
-  "AIzaSyBoHT48I9gj7Thsaltiw3qgphZL7M8Bzf0";
-const NEXT_PUBLIC_MAP_ID = "67dfa868a1478c1c";
+import React, { useState, useEffect } from "react";
 
 const MapPage = () => {
   const [locations, setLocations] = useState([]);
   const [openInfoWindow, setOpenInfoWindow] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBoHT48I9gj7Thsaltiw3qgphZL7M8Bzf0",
+  });
 
   useEffect(() => {
     const fetchLocations = async () => {
@@ -25,21 +23,15 @@ const MapPage = () => {
         const response = await axios.get("http://localhost:3000/locations");
         setLocations(response.data);
       } catch (err) {
-        setError("Error fetching locations");
-      } finally {
-        setLoading(false);
+        console.error(err);
       }
     };
 
     fetchLocations();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-
   const defaultPosition = { lat: 22.501639, lng: 114.128911 };
 
-  // Group locations by latitude and longitude
   const groupedLocations = locations.reduce((acc, location) => {
     const key = `${location.latitude},${location.longitude}`;
     if (!acc[key]) {
@@ -49,57 +41,49 @@ const MapPage = () => {
     return acc;
   }, {});
 
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <>
       <Navbar />
-      <APIProvider apiKey={NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-        <div style={{ height: "calc(100vh - 77px)", width: "100%" }}>
-          <Map
-            zoom={11}
-            center={defaultPosition}
-            mapId={NEXT_PUBLIC_MAP_ID}
-            options={{
-              disableDefaultUI: false,
-              zoomControl: true,
-              draggable: true,
-              scrollwheel: true,
-              panControl: true,
-            }}
-            style={{ height: "100%", width: "100%" }}
-          >
-            {Object.keys(groupedLocations).map((key) => {
-              const [lat, lng] = key.split(",").map(Number);
-              return (
-                <AdvancedMarker
-                  key={key}
-                  position={{ lat, lng }}
-                  onClick={() => setOpenInfoWindow(key)}
-                />
-              );
-            })}
+      <div style={{ height: "calc(100vh - 77px)", width: "100%" }}>
+        <GoogleMap
+          zoom={11}
+          center={defaultPosition}
+          mapContainerStyle={{ height: "100%", width: "100%" }}
+        >
+          {Object.keys(groupedLocations).map((key) => {
+            const [lat, lng] = key.split(",").map(Number);
+            return (
+              <Marker
+                key={key}
+                position={{ lat, lng }}
+                onClick={() => setOpenInfoWindow(key)}
+              />
+            );
+          })}
 
-            {openInfoWindow && (
-              <InfoWindow
-                position={{
-                  lat: groupedLocations[openInfoWindow][0].latitude,
-                  lng: groupedLocations[openInfoWindow][0].longitude,
-                }}
-                onCloseClick={() => setOpenInfoWindow(null)}
-              >
-                <div>
-                  {groupedLocations[openInfoWindow].map((location) => (
-                    <p key={location._id} style={{ margin: "5px 0" }}>
-                      <Link to={`/locations/${location.id}`}>
-                        {location.name}
-                      </Link>
-                    </p>
-                  ))}
-                </div>
-              </InfoWindow>
-            )}
-          </Map>
-        </div>
-      </APIProvider>
+          {openInfoWindow && (
+            <InfoWindow
+              position={{
+                lat: groupedLocations[openInfoWindow][0].latitude,
+                lng: groupedLocations[openInfoWindow][0].longitude,
+              }}
+              onCloseClick={() => setOpenInfoWindow(null)}
+            >
+              <div>
+                {groupedLocations[openInfoWindow].map((location) => (
+                  <p key={location._id} style={{ margin: "5px 0" }}>
+                    <Link to={`/locations/${location.id}`}>
+                      {location.name}
+                    </Link>
+                  </p>
+                ))}
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </div>
     </>
   );
 };
